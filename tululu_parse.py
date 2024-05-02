ff4876606905c32ea4bae698f012915ae53c33e6
lypavel <157053921+lypavel@users.noreply.github.com>
2024-05-02 15:54:53 +0300

    Change .find to .select

--- tululu_parse.py
+++ tululu_parse.py
@@ -5,10 +5,10 @@ from urllib.parse import urljoin
 def parse_page(page_url: str, page_html: str) -> list[str]:
     soup = BeautifulSoup(page_html, 'lxml')
 
-    book_tables = soup.find_all('table', class_='d_book')
+    book_tables = soup.select('.d_book')
     book_urls = []
     for book_table in book_tables:
-        book_url = book_table.find('a')['href']
+        book_url = book_table.select_one('a')['href']
         full_book_url = urljoin(page_url, book_url)
         book_urls.append(full_book_url)
 
@@ -18,21 +18,14 @@ def parse_page(page_url: str, page_html: str) -> list[str]:
 def parse_book_data(book_url: str, book_html: str) -> dict:
     soup = BeautifulSoup(book_html, 'lxml')
 
-    title, author = soup.find('td', class_='ow_px_td')\
-                        .find('h1')\
-                        .text.split(' \xa0 :: \xa0 ')
-    image_url = soup.find('div', class_='bookimage')\
-                    .find('img')['src']
+    title, author = soup.select_one('.ow_px_td h1').text.split(' \xa0 :: \xa0 ')
+
+    image_url = soup.select_one('.bookimage img')['src']
     full_image_url = urljoin(book_url, image_url)
 
-    comments = []
-    div_comments = soup.find_all('div', class_='texts')
-    for div_comment in div_comments:
-        comment = div_comment.find('span', class_='black')
-        comments.append(comment.text)
+    comments = [comment.text for comment in soup.select('.texts .black')]
 
-    span_genres = soup.find('span', class_='d_book').find_all('a')
-    genres = [genre.text for genre in span_genres]
+    genres = [genre.text for genre in soup.select('span.d_book a')]
 
     return {
         'title': title,
