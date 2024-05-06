6b577df63c5e46c44b07d563a3aa358fefa07766
lypavel <157053921+lypavel@users.noreply.github.com>
2024-05-06 18:40:04 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -12,30 +12,40 @@ def load_json(json_path: Path) -> list:
     return books
 
 
-def on_reload() -> None:
+def on_reload(env: Environment, books: list, pages_dir: Path) -> None:
+    books_per_page = 20
+    book_pages = chunked(books, books_per_page)
+
+    for page_num, book_page in enumerate(book_pages):
+
+        books_in_row = 2
+        book_rows = chunked(book_page, books_in_row)
+
+        index_template = env.get_template('index_template.html')
+        rendered_page = index_template.render(
+            book_rows=book_rows
+        )
+
+        pages_dir.mkdir(exist_ok=True)
+        with open(pages_dir / f'index{page_num + 1}.html',
+                  'w',
+                  encoding='utf-8') as index:
+            index.write(rendered_page)
+
+
+if __name__ == '__main__':
     templates_path = Path('templates')
     env = Environment(
         loader=FileSystemLoader(templates_path),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
-    index_template = env.get_template('index_template.html')
+    json_path = Path('downloaded_books.json')
+    books = load_json(json_path)
+    pages_dir = Path('pages')
 
-    books = load_json('downloaded_books.json')
-    books_in_row = 2
-    book_rows = chunked(books, books_in_row)
-
-    rendered_page = index_template.render(
-        book_rows=book_rows
-    )
-
-    with open('index.html', 'w', encoding='utf-8') as index:
-        index.write(rendered_page)
-
-
-if __name__ == '__main__':
-    on_reload()
+    on_reload(env, books)
 
     server = Server()
-    server.watch('templates/*.html', on_reload)
+    server.watch('index_template.html', on_reload)
     server.serve(port=8000, host='0.0.0.0', root='.')
